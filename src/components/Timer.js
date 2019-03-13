026050023e487397ab713a802fc1e9a7ea570eea
MB <manila.bui@gmail.com>
2019-03-12 23:25:57 -0500

    timer responsive text alignment

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,7 +4,9 @@ class Timer extends Component {
   render() {
     return (
       <div className='c-container relative center mw5-ns w-50 mv4 mv5-ns'>
-      	<div className='c-text absolute w-100 f2-ns f3 fw2 tc pa5-ns pa3 mv4-ns mv5 '>00:00</div>
+      	<div className='c-text absolute w-100 f2-ns f3 fw2 tc pa5 mv4-ns mv3 '>
+      		00:00
+      	</div>
         <svg className='w-100' id='c-graphic' viewBox='0 0 100 100'>
 	        <path id='bg' strokeLinecap='round' strokeWidth='4' stroke='#212b37' fill='none' d='M50 2 
 	           a 48 48 0 0 1 0 96 
