d91cf25c09d8e2149b7b1b68a0b46c49359094ad
MB <manila.bui@gmail.com>
2019-03-13 01:40:53 -0500

    added quote state

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ import Footer from './components/Footer';
 import './App.css';
 
 class App extends Component {
+    constructor() {
+    super()
+    this.state = {
+      quote: 'My greatest award is what I’m about to do.'
+    }
+  }
+
   render() {
     return (
       <div>
         <h1 className='f2 fw1 i f1-ns tracked tc'>Wavy Pomo</h1>
-        <Header />
+        <Header quote={this.state.quote}/>
         <Timer />
         <Footer />
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,9 @@ class Header extends Component {
     		<blockquote className="f7 f6-ns i measure-narrow measure-ns center red tr">
 			    <p className="fw3 lh-copy lh-title-ns measure-narrow-ns light-red">
 			    	Ye has something else to tell you, but only if you do some work, because <span className = 'red'>our work is never over.</span>
-			    </p>
-			    <br />
+			    </p><br />
 			    <p className="fw9 lh-copy measure-ns lh-title-ns">
-			    	"My greatest award is what I’m about to do."
+			    	"{ this.props.quote }"
 			    </p>
 			    <cite className="f6-ns f7 ttu tracked fs-normal light-red">―Ye</cite>
 			</blockquote>
